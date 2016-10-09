878257eda65d7c97341b159ad0ae48a18bc0925d
Derry Nixon <nixondc93@gmail.com>
2016-10-09 18:20:20 -0400

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+
+var cardTwo = "king";
+
+var cardThree = "king";
+
+var cardFour = "queen";
+
+
+
+console.log(cardOne === cardTwo);
+console.log(cardOne === cardFour);
+console.log(cardTwo === cardThree);
